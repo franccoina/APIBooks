e06627443d6fcdce883ef9ff6630a9593a13321f
franccoina <franccoina@gmail.com>
2024-07-22 09:16:47 -0500

    feat:Adding controller and models for users manager, and final settings for project styling

--- dist/controllers/controllers.template.js
+++ dist/controllers/controllers.template.js
@@ -2,7 +2,7 @@ export class TemplateController {
     constructor(cardsContainer) {
         this.cardsContainer = cardsContainer;
     }
-    render(id, title, author, description, summary, publicationDate) {
+    renderBooks(id, title, author, description, summary, publicationDate) {
         //Comenzamos definiendo cada elementos con sus atributos y creandolo en el HTML
         const cardDiv = document.createElement('div');
         cardDiv.classList.add('card', 'mb-3');
@@ -29,11 +29,11 @@ export class TemplateController {
         smallSummary.textContent = `📰 ${summary}`;
         smallSummary.classList.add('card-text', 'text-body-secondary');
         const btnUpdate = document.createElement('button');
-        btnUpdate.classList.add('btn', 'btn-update', 'btn-danger', 'me-2');
+        btnUpdate.classList.add('btn', 'btn-update', 'btn-outline-warning', 'me-2');
         btnUpdate.textContent = "Update";
         btnUpdate.setAttribute('data-id', id);
         const btnDelete = document.createElement('button');
-        btnDelete.classList.add('btn', 'btn-delete', 'btn-danger');
+        btnDelete.classList.add('btn', 'btn-delete', 'btn-outline-danger');
         btnDelete.textContent = "Delete";
         btnDelete.setAttribute('data-id', id);
         const pPublicationDate = document.createElement('p');
@@ -47,6 +47,59 @@ export class TemplateController {
         bodyContentDiv.appendChild(pPublicationDate);
         bodyContentDiv.appendChild(btnUpdate);
         bodyContentDiv.appendChild(btnDelete);
+        contentDiv.appendChild(headerContentDiv);
+        contentDiv.appendChild(bodyContentDiv);
+        containerDiv.appendChild(contentDiv);
+        cardDiv.appendChild(containerDiv);
+        this.cardsContainer.classList.add('d-flex', 'justify-content-center', 'flex-wrap', 'gap-5', 'pt-4');
+        this.cardsContainer.appendChild(cardDiv);
+    }
+    renderUsers(id, name, lastName, email, role) {
+        //Comenzamos definiendo cada elementos con sus atributos y creandolo en el HTML
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('card', 'mb-3');
+        cardDiv.setAttribute('max-width', '540px');
+        cardDiv.classList.add('col-md-8');
+        const containerDiv = document.createElement('div');
+        containerDiv.classList.add('row', 'g-0');
+        const contentDiv = document.createElement('div');
+        const headerContentDiv = document.createElement('div');
+        headerContentDiv.classList.add('card-body', 'bg-body-secondary');
+        const bodyContentDiv = document.createElement('div');
+        bodyContentDiv.classList.add('card-body', 'd-flex', 'flex-column');
+        const h4FullName = document.createElement('h4');
+        h4FullName.textContent = `${name} ${lastName}`;
+        h4FullName.classList.add('card-title');
+        const pEmail = document.createElement('p');
+        pEmail.textContent = email;
+        pEmail.classList.add('card-text', 'text-body-secondary');
+        const hr = document.createElement('hr');
+        const smallRole = document.createElement('small');
+        smallRole.classList.add('card-text', 'text-body-secondary');
+        const labelForCheckbox = document.createElement('label');
+        labelForCheckbox.classList.add('mb-2', 'd-flex', 'flex-column', 'border', 'rounded', 'p-3', 'm-3');
+        const checkboxAdminRole = document.createElement('input');
+        checkboxAdminRole.type = "checkbox";
+        checkboxAdminRole.value = "admin_role_checkbox";
+        checkboxAdminRole.classList.add('mt-2');
+        checkboxAdminRole.setAttribute('data-id', id);
+        if (role === 'admin') {
+            smallRole.textContent = `⚙️ Role: ${role}`;
+            labelForCheckbox.textContent = "Revoke Admin Role";
+            checkboxAdminRole.checked = true;
+        }
+        else if (role === 'user') {
+            labelForCheckbox.textContent = "Grant Admin Role";
+            smallRole.textContent = `👤 Role: ${role}`;
+            checkboxAdminRole.checked = false;
+        }
+        //AppendChilds para inyectar al HTML
+        headerContentDiv.appendChild(h4FullName);
+        bodyContentDiv.appendChild(pEmail);
+        bodyContentDiv.appendChild(hr);
+        bodyContentDiv.appendChild(smallRole);
+        bodyContentDiv.appendChild(labelForCheckbox);
+        labelForCheckbox.appendChild(checkboxAdminRole);
         contentDiv.appendChild(headerContentDiv);
         contentDiv.appendChild(bodyContentDiv);
         containerDiv.appendChild(contentDiv);
--- src/controllers/controllers.user.ts
+++ src/controllers/controllers.user.ts
@@ -1,4 +1,5 @@
-import { BodyRequestLogin, BodyResponseLogin } from '../models/model.user';
+import { BodyRequestLogin, BodyResponseLogin, BodyResponseGetUsers, BodyResponseGetById,
+    BodyRequestCreateUsers, BodyResponseCreateUsers, BodyResponseUpdateUsersRole } from '../models/model.user';
 
 export class UsersController {
     public domain: string
@@ -6,6 +7,8 @@ export class UsersController {
     constructor(domain: string) {
         this.domain = domain
     }
+
+    //------------------------------------------- LOGIN (POST) -------------------------------------------
 
     async login(email: HTMLInputElement, password: HTMLInputElement): Promise<BodyResponseLogin> {
         const userData: BodyRequestLogin = {
@@ -39,5 +42,140 @@ export class UsersController {
         const responseBodyLogin: BodyResponseLogin = await response.json() 
 
         return responseBodyLogin;
+    }
+
+    //------------------------------------------- GET ALL -----------------------------------------------------
+
+    async getAllUsers(token: string, limit: number, currentPage: number): Promise<BodyResponseGetUsers> {
+        // se crean los headers con el token para que la API pueda identificar el usuario.
+        const headers: Record<string, string> = {
+            //La API dice que los Headers (-H), deben ser estos, y ahora debemos de enviar el token para
+            //que sea admisible. Es como un 'codigo de administrador' para que la API pueda identificar el usuario
+            'accept': '*/*',
+            'Authorization': `Bearer ${token}`
+        }
+        const reqOptions: RequestInit = {
+            method: 'GET',
+            headers: headers
+        }
+        //Primero ponemos la URL y luego ponemos el objeto con los
+        //parametros del crud (method, headers, body)
+
+        //equivalente al response en crud
+        const response: Response = await fetch(`${this.domain}/api/v1/users?limit=${limit}&page=${currentPage}`, reqOptions)
+        if (!response.ok) {
+            console.log(`Response body: ${(await response.json()).message}`)
+            throw new Error(`HTTP error! status: ${response.status}: ${response.statusText}`)
+        }
+        const responseBodyGetUsers: BodyResponseGetUsers = await response.json()
+        return responseBodyGetUsers
+    }
+
+    //------------------------------------------- FIND (GET ONE BY ID) -----------------------------------------------------
+
+    async findUserById(token: string, userId: string): Promise<BodyResponseGetById> {
+        // se crean los headers con el token para que la API pueda identificar el usuario.
+        const headers: Record<string, string> = {
+            //La API dice que los Headers (-H), deben ser estos, y ahora debemos de enviar el token para
+            //que sea admisible. Es como un 'codigo de administrador' para que la API pueda identificar el usuario
+            'accept': '*/*',
+            'Authorization': `Bearer ${token}`
+        }
+        const reqOptions: RequestInit = {
+            method: 'GET',
+            headers: headers
+        }
+        //Primero ponemos la URL y luego ponemos el objeto con los
+        //parametros del crud (method, headers, body)
+
+        //equivalente al response en crud
+        const response: Response = await fetch(`${this.domain}/api/v1/users/${userId}`, reqOptions)
+        if (!response.ok) {
+            console.log(`Response body: ${(await response.json()).message}`)
+            throw new Error(`HTTP error! status: ${response.status}: ${response.statusText}`)
+        }
+
+        const responseBodyGetById: BodyResponseGetById = await response.json()
+        return responseBodyGetById
+    }
+
+    //------------------------------------------- CREATE (POST) -------------------------------------------
+
+    async createUser(token: string, name: HTMLInputElement, lastName: HTMLInputElement, 
+        email: HTMLInputElement, password: HTMLInputElement): Promise<BodyResponseCreateUsers> {
+        // se crea un objeto con los datos del USER que se quiere crear.
+        //equivalente al request en crud
+        const newUserData: BodyRequestCreateUsers = {
+            name: name.value,
+            lastName: lastName.value,
+            email: email.value,
+            password: password.value
+        }
+        const headers: Record<string, string> = {
+            //La API dice que los Headers (-H), deben ser estos, y ahora debemos de enviar el token para
+            //que sea admisible. Es como un 'codigo de administrador' para que la API pueda identificar el usuario
+            'accept': '*/*',
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
+        const reqOptions: RequestInit = {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(newUserData)
+        }
+        //Primero ponemos la URL y luego ponemos el objeto con los
+        //parametros del crud (method, headers, body)
+
+        //equivalente al response en crud
+        const response: Response = await fetch(`${this.domain}/api/v1/users`, reqOptions)
+
+        //Capturamos el ERROR en caso de que la respuesta no tenga el atributo OK en 'true' (es decir, un 200)
+        if (!response.ok) {
+            console.log(`Response body: ${(await response.json()).message}`)
+            throw new Error(`HTTP error! status: ${response.status}: ${response.statusText}`)
+        }
+
+        // se obtiene la información esperada por la API y se transforma a código...
+        //equivalente a la data en crud
+        const responseBodyCreateUsers: BodyResponseCreateUsers = await response.json()
+        return responseBodyCreateUsers;
+    }
+
+    //NOTA: En esta API, los USERS no pueden ser eliminados.
+
+    //------------------------------------------- UPDATE ROLE (PATCH) -------------------------------------------
+    
+    //En este caso se prefiere usar PATCH en vez del metodo PUT, debido a mayor facilidad, un uso mas directo
+    //y requerimientos de la API
+    // -- En el UPDATE, solo podremos cambiar el role del usuario por determinacion de la API
+
+    async updateUserRole(token: string, userId: string, role: string): Promise<BodyResponseUpdateUsersRole> {
+        // se crea un objeto con los datos del USER que se quieren actualizar (Solo el ROLE, en este caso).
+        
+        const headers: Record<string, string> = {
+            //La API dice que los Headers (-H), deben ser estos, y ahora debemos de enviar el token para
+            //que sea admisible. Es como un 'codigo de administrador' para que la API pueda identificar el usuario
+            'accept': '*/*',
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
+        const reqOptions: RequestInit = {
+            method: 'PATCH',
+            headers: headers
+        }
+        //Primero ponemos la URL y luego ponemos el objeto con los
+        //parametros del crud (method, headers, body)
+
+        //equivalente al response en crud
+        const response: Response = await fetch(`${this.domain}/api/v1/users/${userId}/role?role=${role}`, reqOptions)
+        //Capturamos el ERROR en caso de que la respuesta no tenga el atributo OK en 'true' (es decir, un 200)
+        if (!response.ok) {
+            console.log(`Response body: ${(await response.json()).message}`)
+            throw new Error(`HTTP error! status: ${response.status}: ${response.statusText}`)
+        }
+        // se obtiene la información esperada por la API y se transforma a código...
+        //equivalente a la data en crud
+        const responseBodyUpdateUsersRole: BodyResponseUpdateUsersRole = await response.json()
+        return responseBodyUpdateUsersRole;
     }
 }
